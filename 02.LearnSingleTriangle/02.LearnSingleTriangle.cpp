3cdea9578600ccbf9ffad36562107945bb763ffe
timesu <timesuss@hotmail.com>
2024-04-14 19:54:54 +0900

    Fix 02 Screen background color is balck
    Miss ClearRenderTargetView will cause the screen background color black

--- 02.LearnSingleTriangle/02.LearnSingleTriangle.cpp
+++ 02.LearnSingleTriangle/02.LearnSingleTriangle.cpp
@@ -407,6 +407,15 @@ void OnRender()
     DrawCommandList();
     ExecuteCommandList();
 
+    //PopulateCommandList();
+
+    //// Execute the command list.
+    //ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+    //m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+    //// Present the frame.
+    //m_swapChain->Present(1, 0);
+
     WaitForPreviousFrame();
 }
 
@@ -437,6 +446,9 @@ void SetCommandList()
     CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
     m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
 
+     const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+     m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr); //Screen background color will be black if remove this
+
 }
 
 void DrawCommandList()
@@ -465,9 +477,41 @@ void ExecuteCommandList()
 void PopulateCommandList()
 {
 
+   // // Command list allocators can only be reset when the associated 
+   //// command lists have finished execution on the GPU; apps should use 
+   //// fences to determine GPU execution progress.
+   // m_commandAllocator->Reset();
+
+   // // However, when ExecuteCommandList() is called on a particular command 
+   // // list, that command list can then be reset at any time and must be before 
+   // // re-recording.
+   // m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get());
     ResetCommandList();
+
+    //// Set necessary state.
+    //m_commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+    //m_commandList->RSSetViewports(1, &m_viewport);
+    //m_commandList->RSSetScissorRects(1, &m_scissorRect);
+
+    //// Indicate that the back buffer will be used as a render target.
+    //auto trans = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+    //m_commandList->ResourceBarrier(1, &trans);
+
+    //CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+    //m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
     SetCommandList();
-    
-    DrawCommandList();
+
+    // Record commands.
+   // const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+   // m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+    m_commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    m_commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+    m_commandList->DrawInstanced(3, 1, 0, 0);
+
+    // Indicate that the back buffer will now be used to present.
+    auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+    m_commandList->ResourceBarrier(1, &barrier);
+
+    m_commandList->Close();
 
 }
